49f626cf426eb25fa369a2d42fd6260d6a7e9130
Anna <anna.berejna@gmail.com>
2023-12-15 12:24:31 +0200

    add base page

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -10,7 +10,82 @@ export default class Page {
    * @param path path of the sub page (e.g. /path/to/page.html)
    */
 
-  open(path) {
-    return browser.url(`${path}`);
+  async openURL(path) {
+    await browser.url(path);
+  }
+
+  async getURL() {
+    return browser.getUrl();
+  }
+
+  async waitForTimeout(ms) {
+    await browser.pause(ms);
+  }
+
+  async doesPageHaveURL(url) {
+    await expect(await browser).toHaveUrl(url);
+  }
+
+  async doesPageURLContainText(text) {
+    await expect(await browser.getUrl()).toContain(text);
+  }
+
+  async click(element) {
+    await element.click();
+  }
+
+  async dblClick(element) {
+    await element.doubleClick();
+  }
+
+  async setValue(element, value) {
+    await element.setValue(value);
+  }
+
+  async clearValue(element) {
+    const valueLength = (await element.getValue()).length;
+    for (let i = 0; i < valueLength; i++) {
+      await element.addValue("\uE003");
+    }
+  }
+
+  async addKeyValue(element) {
+    await element.addValue("\uE003");
+  }
+
+  async isDisplayed(element, timeout = 5000) {
+    await expect(await element).toBeDisplayed({ timeout });
+  }
+
+  async isDisabled(element) {
+    await expect(await element).toBeDisabled();
+  }
+
+  async isExist(element) {
+    await expect(await element).not.toBeExisting();
+  }
+
+  async getText(element) {
+    return await element.getText();
+  }
+
+  async checkText(element, text) {
+    const actualText = await this.getText(element);
+    await expect(actualText).toContain(text);
+  }
+
+  async checkTextArray(element, textArray) {
+    const actualText = await this.getText(element);
+    await textArray.forEach((text) => {
+      expect(actualText).toContain(text);
+    });
+  }
+
+  async checkRedHighlightedFields(fields) {
+    for (const field of fields) {
+      const colorProperty = await field.getCSSProperty("border-color");
+      const actualColor = colorProperty.parsed.hex;
+      expect(actualColor).toEqual("#f44336");
+    }
   }
 }
